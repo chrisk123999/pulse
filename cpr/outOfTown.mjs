26b12c44bd29467643aa17630c5d347a4a025e48
Chris <chrisk123999@gmail.com>
2024-03-30 03:29:26 -0700

    Update outOfTown.mjs

--- cpr/outOfTown.mjs
+++ cpr/outOfTown.mjs
@@ -2,4 +2,4 @@ import {EmbedBuilder} from 'discord.js';
 export let cprOutOfTownEmbed = new EmbedBuilder()
     .setColor('DarkGreen')
     .setTitle('Limited Support')
-    .setDescription('There is currently limited support by the module developers. Community support may still be available. Please carefully read through <#1118326768126926909> and <#1098146764386730015> as it solves most common issues. Take take basic troubleshooting steps, such as disabling all non-CPR module dependencies, test with SRD actors, and restarting your client/server.');
+    .setDescription('There is currently limited support by the module developers. Community support may still be available. Please carefully read through <#1118326768126926909> and <#1098146764386730015> as it solves most common issues. Take the following basic troubleshooting steps: Disable all modules except CPR and it\'s dependencies, test with SRD actors, and restart your client/server.');
